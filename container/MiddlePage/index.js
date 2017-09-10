66b621f8f7457a8f768542b290cadbf6cf65873e
PVer <475159287@qq.com>
2017-09-10 19:58:17 +0800

    上传项目

---
+++ container/MiddlePage/index.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+import {Link} from 'react-router-dom';
+import './index.less';
+export default class Middle extends Component{
+    render(){
+        return (
+            <div className="login_All">
+                <div className="login_Logo">
+                    <img src="http://p3.music.126.net/tBTNafgjNnTL1KlZMt7lVA==/18885211718935735.jpg" alt="#" className="logo_Img"/>
+                    <div className="iphone_input">
+                        <Link to='/Login/index' className="user_button">手机号登录</Link>
+                        <Link to='/logon/index' className="user_button">注册</Link>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+}
